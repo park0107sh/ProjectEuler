8ad81ef764e36bd36f7d0914383249716edbf1eb
Soowhan <park0107sh@gmail.com>
2018-01-14 21:03:58 -0800

    miss test 1 & 2

---
+++ pe4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int is_palind(unsigned int n)
+{
+		unsigned int reversed = 0, t = n;
+
+		while (t) {
+				reversed = 10*reversed + (t % 10);
+				t /= 10;
+		}
+		return reversed == n;
+}
+
+int main(void)
+{
+	unsigned int T, k, N, i, j, p = 0, max = 0;
+	scanf("%d ", &T);
+	for (k=0; k<T; k++) {
+		scanf("%d ", &N);
+		for (i=100; i<=999; i++) {
+			for (j=100; j<=999; j++) {
+				p = i*j;
+				if (p < N) {
+					if (is_palind(p) && p > max)
+							max = p;
+				}
+			}
+		}
+		printf("%u\n", max);
+	}
+	return 0;
+}
